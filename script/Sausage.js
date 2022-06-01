5b1386a08ea54e47526a8d60594f7c5c22b7f537
Nia <verusnikus@gmail.com>
2022-06-01 19:12:13 +0300

    Добавление некоторых стилей

--- script/Sausage.js
+++ script/Sausage.js
@@ -1,3 +1,9 @@
 /*колбаса правит миром...
 текст написан в ПростоБесполезныйТекст*/
 
+let text_field = document.getElementById("text");
+let first_choice = document.getElementById("firstChoice");
+let second_choice = document.getElementById("secondChoice");
+let timer = 10;
+let wasted = false;
+let open_refrigerator = false;
