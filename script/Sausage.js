49200ff1115040dcf229fecc85c9b9325ea44fa1
Nia <verusnikus@gmail.com>
2022-06-01 22:13:55 +0300

    Изменение if на switch

--- script/Sausage.js
+++ script/Sausage.js
@@ -18,7 +18,7 @@ function gameOver() {
     // document.body.style.background = "brown";
 }
 function OnClick(e) {
-    /*switch (stage) {
+    switch (stage) {
         case "mom's task":
             if (e.target.id == "firstChoice") {
                 stage = "agree?"
@@ -42,64 +42,98 @@ function OnClick(e) {
             }
             break;
         case "open fridge":
+            if (e.target.id == "firstChoice") {
+                text_field.innerText = "*Ти стоїшь перед холодильником*";
+                first_choice.innerText = "Відкрити";
+                second_choice.innerText = "Дивитись на холодильник сумними очима, чогось очікуючи"
+                stage = "take sausage"
+            } else {
+                hide()
+                setTimeout(gameOver, 3000);
+                text_field.innerText = "*Ти стоїшь... Нічого не відб- .\n Тобі дали тапком по голові*";
+            }
+            break;
+        case "take sausage":
+            if (e.target.id == "firstChoice") {
+                text_field.innerText = "*Ковбаса спокійно лежить на другій поличці*";
+                first_choice.innerText = "Взяти, порізати й залишити на столі";
+                second_choice.innerText = "Взяти та з'їсти"
+                stage = "final"
+            } else {
+                hide();
+                setTimeout(gameOver, 5000);
+                text_field.innerText = "Навіть якщо дивитись на холодильник годину, сам по собі він не відкриється. \n Твої дії тільки розізлили матір."
+            }
+            break;
+        default:
+            if (e.target.id == "firstChoice") {
+                hide();
+                setTimeout(gameOver, 5000);
+                text_field.innerText = "Через деякий час прийшов тато та ковбаси не стало. \n Мати дала тапком по голові тату й тобі за одно.";
+            } else {
+                hide();
+                setTimeout(gameOver, 3000);
+                text_field.innerText = "Якщо ти такий голодний треба було просто сказати мамі, а не їсти те, що вона попросила залишити."
+            }
+            break;
 
-
-    } */
-    if (stage == "mom's task") {
-        if (e.target.id == "firstChoice") {
-            stage = "agree?"
-            text_field.innerText = "Сонечко, допоможи матусі. Поріж ковбасу, будь ласка, щоб я змогла зробити вам піцу."
-            first_choice.innerText = "Добре.";
-            second_choice.innerText = "Не хочу, сама ріж, в мене справи."
-        } else {
-            hide()
-            text_field.innerText = "..."
-            setTimeout(gameOver, 2000)
-        }
-    } else if (stage == "agree?") {
-        if (e.target.id == "firstChoice") {
-            text_field.innerText = "Люблю тебе.";
-            first_choice.innerText = "Піти до холодильника";
-            second_choice.innerText = "Просто стояти"
-            stage = "open fridge"
-        } else {
-            hide()
-            setTimeout(gameOver, 3000);
-            text_field.innerText = "Ясно, як завжди, усе потрібно робити самій."
-        }
-    } else if (stage == "open fridge") {
-        if (e.target.id == "firstChoice") {
-            text_field.innerText = "*Ти стоїшь перед холодильником*";
-            first_choice.innerText = "Відкрити";
-            second_choice.innerText = "Дивитись на холодильник сумними очима, чогось очікуючи"
-            stage = "take sausage"
-        } else {
-            hide()
-            setTimeout(gameOver, 3000);
-            text_field.innerText = "*Ти стоїшь... Нічого не відб- .\n Тобі дали тапком по голові*";
-        }
-    } else if (stage == "take sausage") {
-        if (e.target.id == "firstChoice") {
-            text_field.innerText = "*Ковбаса спокійно лежить на другій поличці*";
-            first_choice.innerText = "Взяти, порізати й залишити на столі";
-            second_choice.innerText = "Взяти та з'їсти"
-            stage = "final"
-        } else {
-            hide();
-            setTimeout(gameOver, 5000);
-            text_field.innerText = "Навіть якщо дивитись на холодильник годину, сам по собі він не відкриється. \n Твої дії тільки розізлили матір."
-        }
-    } else {
-        if (e.target.id == "firstChoice") {
-            hide();
-            setTimeout(gameOver, 5000);
-            text_field.innerText = "Через деякий час прийшов тато та ковбаси не стало. \n Мати дала тапком по голові тату й тобі за одно.";
-        } else {
-            hide();
-            setTimeout(gameOver, 3000);
-            text_field.innerText = "Якщо ти такий голодний треба було просто сказати мамі, а не їсти те, що вона попросила залишити."
-        }
     }
+
+    // if (stage == "mom's task") {
+    //     if (e.target.id == "firstChoice") {
+    //         stage = "agree?"
+    //         text_field.innerText = "Сонечко, допоможи матусі. Поріж ковбасу, будь ласка, щоб я змогла зробити вам піцу."
+    //         first_choice.innerText = "Добре.";
+    //         second_choice.innerText = "Не хочу, сама ріж, в мене справи."
+    //     } else {
+    //         hide()
+    //         text_field.innerText = "..."
+    //         setTimeout(gameOver, 2000)
+    //     }
+    // } else if (stage == "agree?") {
+    //     if (e.target.id == "firstChoice") {
+    //         text_field.innerText = "Люблю тебе.";
+    //         first_choice.innerText = "Піти до холодильника";
+    //         second_choice.innerText = "Просто стояти"
+    //         stage = "open fridge"
+    //     } else {
+    //         hide()
+    //         setTimeout(gameOver, 3000);
+    //         text_field.innerText = "Ясно, як завжди, усе потрібно робити самій."
+    //     }
+    // } else if (stage == "open fridge") {
+    //     if (e.target.id == "firstChoice") {
+    //         text_field.innerText = "*Ти стоїшь перед холодильником*";
+    //         first_choice.innerText = "Відкрити";
+    //         second_choice.innerText = "Дивитись на холодильник сумними очима, чогось очікуючи"
+    //         stage = "take sausage"
+    //     } else {
+    //         hide()
+    //         setTimeout(gameOver, 3000);
+    //         text_field.innerText = "*Ти стоїшь... Нічого не відб- .\n Тобі дали тапком по голові*";
+    //     }
+    // } else if (stage == "take sausage") {
+    //     if (e.target.id == "firstChoice") {
+    //         text_field.innerText = "*Ковбаса спокійно лежить на другій поличці*";
+    //         first_choice.innerText = "Взяти, порізати й залишити на столі";
+    //         second_choice.innerText = "Взяти та з'їсти"
+    //         stage = "final"
+    //     } else {
+    //         hide();
+    //         setTimeout(gameOver, 5000);
+    //         text_field.innerText = "Навіть якщо дивитись на холодильник годину, сам по собі він не відкриється. \n Твої дії тільки розізлили матір."
+    //     }
+    // } else {
+    //     if (e.target.id == "firstChoice") {
+    //         hide();
+    //         setTimeout(gameOver, 5000);
+    //         text_field.innerText = "Через деякий час прийшов тато та ковбаси не стало. \n Мати дала тапком по голові тату й тобі за одно.";
+    //     } else {
+    //         hide();
+    //         setTimeout(gameOver, 3000);
+    //         text_field.innerText = "Якщо ти такий голодний треба було просто сказати мамі, а не їсти те, що вона попросила залишити."
+    //     }
+    // }
 }
 
 first_choice.addEventListener("click", (ev) => { OnClick(ev) })
