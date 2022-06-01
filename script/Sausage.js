4c3923cea707f2b50176bf44cce85d3c94158e13
Nia <verusnikus@gmail.com>
2022-06-01 21:25:12 +0300

    Первая стадия развилки

--- script/Sausage.js
+++ script/Sausage.js
@@ -4,6 +4,29 @@
 let text_field = document.getElementById("text");
 let first_choice = document.getElementById("firstChoice");
 let second_choice = document.getElementById("secondChoice");
+let stage = "mom's task";
+let wasted = false, open_refrigerator = false;
 let timer = 10;
-let wasted = false;
-let open_refrigerator = false;
+
+function gameOver() {
+    text_field.style.display = "none";
+    first_choice.style.display = "none";
+    second_choice.style.display = "none";
+    document.body.innerHTML = "Wasted";
+    // document.body.style.background = "brown";
+}
+function OnClick(e) {
+    if (stage == "mom's task") {
+        if (e.target.id == "firstChoice") {
+            stage = "agree?"
+            text_field.innerHTML = "Сонечко, допоможи матусі. Поріж ковбасу, будь ласка, щоб я змогла зробити вам піцу."
+            first_choice.innerText = "Добре.";
+            second_choice.innerText = "Не хочу, сама ріж, в мене справи."
+        } else {
+            gameOver();
+        }
+    }// else if () {}
+}
+
+first_choice.addEventListener("click", (ev) => {OnClick(ev)})
+second_choice.addEventListener("click", (ev) => {OnClick(ev)})
